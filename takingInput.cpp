0aa564f6955783090abfd6b233301ce6e873cd62
Amit Kumar <kumarroy9798@gmail.com>
2023-12-17 19:24:00 +0530

    Create takingInput.cpp

---
+++ takingInput.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{        int num;
+         cout << "Enter num : ";
+         cin>>num;
+         cout<<"You entered : "<<num;
+
+    return 0;
+}
